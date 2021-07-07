43cfefaf9ed016e2c86672e1bf7d60165bcd4ea9
shikhakuthiala28 <75114065+shikhakuthiala28@users.noreply.github.com>
2021-07-07 18:43:56 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,42 @@
+class Boat {
+  constructor(x, y,width,height,boatpos,boatAnimation) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+    };
+    
+    this.animation=boatAnimation;
+    this.speed=0.05;
+    this.body = Bodies.rectangle(x, y, width,height, options);
+    this.w=width;
+    this.h=height;
+    this.boatposition=boatpos;
+    
+
+    this.image = loadImage("assets/boat.png");
+    
+    World.add(world, this.body);
+  }
+
+  animate(){
+    this.speed=this.speed+0.05%1.1;
+  }
+
+  remove(index){
+    Matter.World.remove(world,boats[index].body);
+    boats.splice(index,1);
+  }
+  display() {
+      var angle = this.body.angle;
+      var pos = this.body.position;
+      var index= floor(this.speed%this.animation.length);
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.animation[index], 0, this.boatposition, this.w,this.h);
+      pop();
+
+  }
+}
